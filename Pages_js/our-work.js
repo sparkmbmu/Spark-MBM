c3c688761d3f4021afe9c03a13f0c8e39346217f
shekhawattanvi <shekhawattanvi2802@gmail.com>
2025-08-13 00:10:29 +0530

    minor updates

--- Pages_js/our-work.js
+++ Pages_js/our-work.js
@@ -256,3 +256,27 @@ document.addEventListener('DOMContentLoaded', () => {
         document.addEventListener('DOMContentLoaded', () => {
             document.getElementById('Contests').style.display = "block";
         });
+
+let prevScroll = window.scrollY;
+const navbar = document.getElementById("navbar");
+const navbarHeight = navbar.offsetHeight;
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.scrollY;
+
+  if (currentScroll <= navbarHeight) {
+    navbar.style.top = "0";
+  } else if (currentScroll > prevScroll) {
+    navbar.style.top = `-${navbarHeight}px`;
+  } else {
+    navbar.style.top = "0";
+  }
+
+  prevScroll = currentScroll;
+
+  if (window.scrollY > 10) {
+  navbar.classList.add("scrolled");
+} else {
+  navbar.classList.remove("scrolled");
+}
+});
