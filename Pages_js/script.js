8dd822038265232d42728625132127b49c6df624
Ome Tiwari <omt81144@gmail.com>
2025-08-16 15:18:49 +0530

    changes

--- Pages_js/script.js
+++ Pages_js/script.js
@@ -73,7 +73,9 @@ document.addEventListener("DOMContentLoaded", () => {
       bar.style.opacity = 1;
     }, 500);
   }
-  const words = ["ANALYZE", "CODE", "CONQUER"];
+  // const words = ["ANALYZE", "CODE", "CONQUER"];
+  const words = ["AUTOMATE", "OPTIMIZE", "DEPLOY"];
+
   let currentWordIndex = 0;
   let charIndex = 0;
   let isDeleting = false;
